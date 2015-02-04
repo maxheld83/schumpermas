3ad21b356a809f9f9800ab64147bdeb78584d90a
Maximilian Held <maximilian.held83@gmail.com>
2015-02-04 11:23:38 +0100

    add import stuff

--- r/imports.R
+++ r/imports.R
@@ -26,7 +26,7 @@ q.distribution <- as.integer(c(  # set up distribution
 q.sampling.structure <- read.csv(  # read in sampling structure
   file = "keyneson/keyneson-sample/sampling-structure.csv"
 )
-q.sample <- q.sampling.structure$handle  # exctract vector of sel. handles
+q.sample <- q.sampling.structure$handle  # extract vector of sel. handles
 q.set <- build.q.set( #  build q.set
   q.concourse = q.concourse,
   q.sample = q.sample,
@@ -41,7 +41,7 @@ q.sorts <- import.q.sorts(  # now import the sorts
     read.csv(
       "keyneson/keyneson-sample/keyneson-concourse/ids.csv",
       row.names=2
-    )  
+    )
   )
 )
 kicked.out.part <- c(
@@ -60,4 +60,11 @@ q.feedback <- import.q.feedback(
       row.names=2
     )
   )
+)
+keyneson <- list("before"=c(), "after"=c())
+keyneson$before <- qmethod(
+  dataset = q.sorts[,,"before"],
+  nfactors = 3,
+  rotation = "varimax",
+  forced = TRUE
 )
