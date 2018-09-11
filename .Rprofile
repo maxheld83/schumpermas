cdbc16cab85734a983d6e2215496e882a34b463f
Maximilian Held <info@maxheld.de>
2018-09-11 16:20:49 +0200

    migrate to bookdown closes #333

---
+++ .Rprofile
@@ -0,0 +1,4 @@
+if (isTRUE(interactive() & Sys.getenv("RSTUDIO") == 1)) {
+  # source only when we're in rstudio
+  source(file = "setup.R")
+}
---
+++ setup.R
@@ -0,0 +1,23 @@
+# knitr options ====
+knitr::opts_chunk$set(
+  tidy = TRUE,   # tidy formats code nicely in echo
+  cache = TRUE,
+  echo = FALSE,
+  message = FALSE
+)
+options(digits = 2)  # display only 2 digits in knitr output
+options(scipen = 999)
+options(knitr.kable.NA = '')
+
+
+# dependencies ====
+# automatically create a bib database for R packages
+knitr::write_bib(
+  x = c(
+    .packages(),
+    'bookdown',
+    'knitr',
+    'rmarkdown'
+  ),
+  file = 'packages.bib'
+)
